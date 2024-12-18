71d2269ebaeb3e8829d778c2956f800f7bef3d04
Srimanjunath23 <rsm2304r@gmail.com>
2024-12-18 20:27:44 +0530

    Day 1 intial setup with required installations

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+//react alick for carousel
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+  <Routes>
+    {/* <Route path='/' element={<Homepage/>}/>
+    <Route path='/movie/id:' element={<Moviepage/>}/>
+    <Route path='plays/' element={<Plays/>}/> */}
+  </Routes>
+  );
+}
+
+export default App;
